93a8bf80f6fd19c76820423391eb90f236cd4aca
Derrick Cudd <instantramen125@gmail.com>
2017-03-31 03:18:03 -0400

    clean code

--- app.rb
+++ app.rb
@@ -1,22 +1,8 @@
 require "sinatra"
+require "sinatra/reloader"
 require "./tranship_changer.rb"
 
 set :public_folder, File.dirname(__FILE__) + '/static'
-
-pro_numbers = %q{
-1234567
-1113344
-2222333
-2333455
-}
-
-def split!(string)
-  res = []
-  string.split("\n").each do |line|
-    res << line if line.length > 1
-  end
-  res
-end
 
 def change_tranships(pros_string, tranships)
   pros = []
@@ -27,13 +13,10 @@ def change_tranships(pros_string, tranships)
   tranship_changer = TranshipChanger.new(pros)
   tranship_changer.append_tranships(tranships)
 end
-# result = change_tranships(pro_numbers, "kearny")
-# puts result
 
 get "/" do
   if params["pros"]
     pros = params["pros"]
-    #.gsub (/\r\n?/, "\n")
     pros.gsub!(/\r\n?/, "\n")
     result = change_tranships(pros, params["tranship"])
   end
