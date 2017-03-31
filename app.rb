1dca7e9c16f94cd184f97e6414618a20973629e6
Derrick Cudd <instantramen125@gmail.com>
2017-03-31 03:15:49 -0400

    finish tranship changer

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
-require("./tranship_changer.rb")
+require "sinatra"
+require "./tranship_changer.rb"
+
+set :public_folder, File.dirname(__FILE__) + '/static'
 
 pro_numbers = %q{
 1234567
@@ -7,7 +10,32 @@ pro_numbers = %q{
 2333455
 }
 
-pros = pro_numbers.split("\n")
-tranship_changer = TranshipChanger.new(pros)
-final = tranship_changer.append_tranships("803,779,776")
-puts final
+def split!(string)
+  res = []
+  string.split("\n").each do |line|
+    res << line if line.length > 1
+  end
+  res
+end
+
+def change_tranships(pros_string, tranships)
+  pros = []
+  tranships ||= "kearny"
+  pros_string.split(/(?:\n\r?|\r\n)/).each do |pro|
+    pros << pro if pro.length > 1
+  end
+  tranship_changer = TranshipChanger.new(pros)
+  tranship_changer.append_tranships(tranships)
+end
+# result = change_tranships(pro_numbers, "kearny")
+# puts result
+
+get "/" do
+  if params["pros"]
+    pros = params["pros"]
+    #.gsub (/\r\n?/, "\n")
+    pros.gsub!(/\r\n?/, "\n")
+    result = change_tranships(pros, params["tranship"])
+  end
+  erb(:index, locals: {result: result})
+end
--- tranship_changer.rb
+++ tranship_changer.rb
@@ -1,13 +1,23 @@
 class TranshipChanger
   @pros = []
+  @@valid_tranships = {
+    kearny: "803,679,885",
+    tomsbrook: "803,779,776"
+  }
   def initialize(pros)
     @pros = pros
   end
 
   def append_tranships(tranship_string)
+    tranship_string.downcase!
+    if @@valid_tranships[tranship_string.to_sym]
+      tranships = @@valid_tranships[tranship_string.to_sym]
+    else
+      tranships = tranship_string
+    end
     result = ""
     @pros.each do |pro|
-      result << "#{pro} \n2 \n#{tranship_string} \n \n \n"
+      result << "#{pro} \n2 \n#{tranships} \n \n \n"
     end
     result
   end
