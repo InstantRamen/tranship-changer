d207bd6a7350a28f85c5fad2cf4992f6c4d004a9
Derrick Cudd <instantramen125@gmail.com>
2017-04-01 00:16:04 -0400

    clean code

--- tranship_changer.rb
+++ tranship_changer.rb
@@ -6,17 +6,22 @@ class TranshipChanger
     mansfield: "803,779,776",
     austintown: "803,776"
   }
+
   def initialize(pros)
     @pros = pros
   end
 
-  def append_tranships(tranship_string)
+  def validate_tranship(tranship_string)
     tranship_string.downcase!
     if @@valid_tranships[tranship_string.to_sym]
-      tranships = @@valid_tranships[tranship_string.to_sym]
+      @@valid_tranships[tranship_string.to_sym]
     else
-      tranships = tranship_string
+      tranship_string
     end
+  end
+
+  def append_tranships(tranship_string)
+    tranships = validate_tranship(tranship_string)
     result = ""
     @pros.each do |pro|
       result << "#{pro} \n2 \n#{tranships} \n \n \n \n"
