2084aac6a4734a411f1ad5b30625af364254affd
Derrick Cudd <instantramen125@gmail.com>
2017-04-03 17:15:50 -0400

    make more object oriented and switch to POST instead of GET

---
+++ app.rb
@@ -0,0 +1,40 @@
+require "sinatra/base"
+require "./tranship_changer.rb"
+
+class App < Sinatra::Base
+  @pros = []
+  @result = "nothing is here..."
+  set :public_folder, File.dirname(__FILE__) + '/static'
+
+  def change_tranships(pros_string, tranships)
+    pros = []
+    tranships ||= "kearny"
+    pros_string.split(/(?:\n\r?|\r\n)/).each do |pro|
+      pros << pro if pro.length > 1
+    end
+    tranship_changer = TranshipChanger.new(pros)
+    tranship_changer.append_tranships(tranships)
+  end
+
+  get "/" do
+    if params["pros"]
+      @pros = params["pros"]
+      @pros.gsub!(/^\s*$/, "")
+      @pros.gsub!(/\r\n?/, "\n")
+      @result = change_tranships(@pros, params["tranship"])
+    end
+    erb(:index, locals: {result: @result})
+  end
+
+  post "/" do
+    if params["pros"]
+      @pros = params["pros"]
+      @pros.gsub!(/^\s*$/, "")
+      @pros.gsub!(/\r\n?/, "\n")
+      @result = change_tranships(@pros, params["tranship"])
+    end
+    erb(:index, locals: {result: @result})
+  end
+
+  run!
+end
